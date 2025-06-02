6e37d58dd0e4acfd9d571cd1f43f5acf3d9330a4
Lina Parashchenko <blond2109@gmail.com>
2025-06-02 17:26:07 +0300

    Initial commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,35 @@
+import css from "./App.module.css";
+import { useSelector, useDispatch } from "react-redux";
+import { useMemo } from "react";
+import { useDebounce } from "use-debounce";
+
+import ContactForm from "../ContactForm/ContactForm";
+import ContactList from "../ContactList/ContactList";
+import SearchBox from "../SearchBox/SearchBox";
+import { deleteContact } from "../../redux/contactsSlice";
+
+export default function App() {
+  const contacts = useSelector((state) => state.contacts.items);
+  const filter = useSelector((state) => state.filters.name);
+  const [debouncedInputValue] = useDebounce(filter, 200);
+  const dispatch = useDispatch();
+
+  const visibileContacts = useMemo(() => {
+    return contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(debouncedInputValue.toLowerCase())
+    );
+  }, [debouncedInputValue, contacts]);
+
+  const handleDelete = (id) => {
+    dispatch(deleteContact(id));
+  };
+
+  return (
+    <div className={css.container}>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList contactsList={visibileContacts} onDelete={handleDelete} />
+    </div>
+  );
+}
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,29 @@
+import css from "./ContactList.module.css";
+import Contact from "../Contact/Contact";
+import { useSelector } from "react-redux";
+import { useDebounce } from "use-debounce";
+import { useMemo } from "react";
+
+export default function ContactList() {
+  const contacts = useSelector((state) => state.contacts.items);
+  const filter = useSelector((state) => state.filters.name);
+  const [debouncedInputValue] = useDebounce(filter, 200);
+
+  const visibleContacts = useMemo(() => {
+    return contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(debouncedInputValue.toLowerCase())
+    );
+  }, [contacts, debouncedInputValue]);
+
+  return (
+    <div className={css.wrap}>
+      <ul className={css.listContact}>
+        {visibleContacts.map((contact) => (
+          <li key={contact.id} className={css.itemBorder}>
+            <Contact contact={contact} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
